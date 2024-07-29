089ac7aa5a009cb48713aa90a2a01a97b4f2b59e
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2024-07-29 11:57:07 -0400

    Update example calculation

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,23 @@
 using namespace std;
 
 int main() {
-  using std::chrono::duration;
-  using std::chrono::duration_cast;
-  using std::chrono::high_resolution_clock;
-  using std::chrono::milliseconds;
+    using std::chrono::duration;
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
 
-  ofstream file("svp_symmetric.txt");
+    ofstream file("svp_symmetric_9.txt");
 
-  auto t1 = high_resolution_clock::now();
+    auto t1 = high_resolution_clock::now();
 
-  for (int p = 2; p < 1000; p++) {
-    svp_symmetric(p, exact_svp, 5, file);
-  }
-  
-  auto t2 = high_resolution_clock::now();
+    // iterate over p in [2, 1000)
+    for (int p = 2; p < 1000; p++) {
+        svp_sym(p, exact_svp, 9, file);
+    }
 
-  return 0;
+    auto t2 = high_resolution_clock::now();
+
+    cout << "Time: " << duration_cast<milliseconds>(t2 - t1).count() / 1000.0 << "s" << endl;
+
+    return 0;
 }
