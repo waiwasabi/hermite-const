0d72fd5201d79b71ea82ba6c1a0ee8876199c818
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2024-07-29 11:14:09 -0400

    Add function to initialize U(p) vector with given center and radius

--- src/hlawka.cpp
+++ src/hlawka.cpp
@@ -1,45 +1,79 @@
 #include "hlawka.h"
 
-namespace hlawka {
+namespace Hlawka {
 /**
  * @brief Initialize a vector in U(p)
  */
 void U(Vec<ZZ> &a, int N) {
-  a.SetLength(N);
-  a[0] = 1;
+    a.SetLength(N);
+    a[0] = 1;
 }
 
-void increment(Vec<ZZ> &a, int p, int N) {
-  for (int i = N - 1; i >= 0; i--) {
-    if (a[i] < p - 1) {
-      a[i]++;
-      return;
+/**
+ * @brief Initialize a vector in U(p) centered at c with radius r
+ */
+void U_center(Vec<ZZ> &a, Vec<ZZ> &c, ZZ r, int N) {
+    a.SetLength(N);
+    a[0] = 1;
+    for (int i = 1; i < N; i++) {
+        a[i] = c[i] - r;
     }
-    a[i] = 0;
-  }
-}
-
-void symmetric_increment(Vec<ZZ> &a, int p, int N) {
-  for (int i = N - 1; i > 1; i--) {
-    if (a[i] < a[i - 1] - 1) {
-      a[i]++;
-      return;
-    }
-    a[i] = 0;
-  }
-  a[1]++;
 }
 
 /**
  * @brief Initialize the lattice basis B(p, a)
  */
 void B(Mat<ZZ> &B, int p, Vec<ZZ> a, int N) {
-  B = ident_mat_ZZ(N) * p;
-  B[0] = a;
+    B = ident_mat_ZZ(N) * p;
+    B[0] = a;
 }
 
 /**
  * @brief Compute q given p
  */
-double q(int p, int N) { return pow(p, (N - 1.) / N); }
-}  // namespace hlawka
+double q(int p, int N) {
+    return pow(p, (N - 1.) / N);
+}
+
+/**
+ * @brief Increment a vector in U(p)
+ */
+void incr(Vec<ZZ> &a, int p, int N) {
+    for (int i = N - 1; i >= 0; i--) {
+        if (a[i] < p - 1) {
+            a[i]++;
+            return;
+        }
+        a[i] = 0;
+    }
+}
+
+/**
+ * @brief Increment a vector in the symmetric subdivision of U(p)
+ */
+void sym_incr(Vec<ZZ> &a, int, int N) {
+    for (int i = N - 1; i > 1; i--) {
+        if (a[i] < a[i - 1] - 1) {
+            a[i]++;
+            return;
+        }
+        a[i] = 0;
+    }
+    a[1]++;
+}
+
+/**
+ * @brief Increment a vector in the symmetric subdivision of U(p) within the
+ * hypercube centered at c with radius r
+ */
+void symc_incr(Vec<ZZ> &a, int, Vec<ZZ> &c, ZZ &r, int N) {
+    for (int i = N - 1; i > 1; i--) {
+        if (a[i] < a[i - 1] - 1 && a[i] < c[i] + r) {
+            a[i]++;
+            return;
+        }
+        a[i] = c[i] - r;
+    }
+    a[1]++;
+}
+}  // namespace Hlawka
