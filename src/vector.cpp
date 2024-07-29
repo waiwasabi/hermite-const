b5104d00d8f8b705e724c6b8d4104c8803b2b00c
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2024-07-29 11:17:18 -0400

    Add utility function for double[] to NTL::Vec<RR> conversion

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,12 +1,25 @@
+#include <NTL/RR.h>
 #include <hlawka.h>
 
 /**
  * @brief Compute the norm of a vector
  */
 double norm(Vec<ZZ> a) {
-  double res = 0;
-  for (int i = 0; i < a.length(); i++) {
-    res += conv<int>(a[i] * a[i]);
-  }
-  return sqrt(res);
+    double res = 0;
+    for (int i = 0; i < a.length(); i++) {
+        res += conv<int>(a[i] * a[i]);
+    }
+    return sqrt(res);
+}
+
+/**
+ * @brief Convert an array to an NTL vector
+ */
+Vec<RR> arr_to_vec(double *arr, int N) {
+    Vec<RR> res;
+    res.SetLength(N);
+    for (int i = 0; i < N; i++) {
+        res[i] = arr[i];
+    }
+    return res;
 }
